34cbf1b9ce838873a4120f4a9f35952103f863b7
Jiha-cloud <67041677+Jiha-cloud@users.noreply.github.com>
2021-04-30 17:16:53 +0900

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package java_20210429;
-// font 바꾸는 방법 Window->preference->"font" 검색->
-// general-appearance-Colors and Font->Java->
-// Java Editor Text Font-Edit
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
-}
