c4f5ded2b8be4e31e464f44ae0de68bdbb4ffc8a
Jiha-cloud <67041677+Jiha-cloud@users.noreply.github.com>
2021-05-04 17:47:48 +0900

    Add files via upload

--- java_20210429/HelloWorld.java
+++ java_20210429/HelloWorld.java
@@ -1,4 +1,5 @@
 package java_20210429;
+// 안녕하세요....
 // font 바꾸는 방법 Window->preference->"font" 검색->
 // general-appearance-Colors and Font->Java->
 // Java Editor Text Font-Edit
--- java_20210504/CalendarDemo.java
+++ java_20210504/CalendarDemo.java
@@ -1,26 +1,28 @@
-package java_20210430;
+package java_20210504;
 
-public class Homework {
+public class CalendarDemo {
+
 	public static void main(String[] args) {
-		// 윤년 : 4의 배수 중에서 100의 배수 제외, 이 중 400의 배수는 윤년
-		// 1년 1월 1일은 월요일
-		// 2020*365 + 윤년햇수
-
-		/*
-		 * 달력만들기 1. 1년 1월 1일은 월요일 2. 1년은 365일이고, 윤년은 366일 - 2월 29일 3. 윤년은 4년마다 발생하고,
-		 * 그중에서 100의 배수는 제외 400의 배수는 윤년 2021년 4월 30일은 무슨 요일일까요?
-		 * 
-		 * 
-		 * 
-		 * 
-		 */
-
+		// TODO Auto-generated method stub
 		int year = 2021;
-		int month = 4;
-		int day = 30;
+		int month = 5;
+		int day = 4;
 
-		int leapYear = (year-1) / 4 - (year-1) / 100 + (year-1) / 400;
-		int numOfDay = (year-1) * 365 + leapYear + 31 + 28 + 31 + 30; //2021년 4월 30일까지의 총일수
+		int leapYear = (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400;
+		int[] monthArray = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		// and문이 or문보다 연산순서가 앞서므로 괄호가 필요없긴함.
+		boolean isLeafYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0; // 앞에 and문이 false여도 뒤에 조건이 true이므로
+																					// 전체 or문은 true.
+		if (isLeafYear) {
+			monthArray[1] = 29;
+		}
+		int numOfDay = (year - 1) * 365 + leapYear; // 2021년 4월 30일까지의 총일수
+		for (int i = 0; i < month - 1; i++) {
+			numOfDay += monthArray[i];
+
+		}
+		numOfDay += day;
+
 		if (numOfDay % 7 == 1) {
 			System.out.printf("%d년 %d월 %d일은 월요일입니다.%n", year, month, day);
 		} else if (numOfDay % 7 == 2) {
@@ -37,5 +39,4 @@ public class Homework {
 			System.out.printf("%d년 %d월 %d일은 일요일입니다.%n", year, month, day);
 		}
 	}
-
 }
